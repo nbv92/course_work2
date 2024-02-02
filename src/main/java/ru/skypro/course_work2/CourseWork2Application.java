c9713c487dca03ac7840fc3d6ce7d1671aafa176
Timofey <timofey.sharipov@gmail.com>
2024-02-02 22:47:12 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/course_work2/CourseWork2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.course_work2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/course_work2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.course_work2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
